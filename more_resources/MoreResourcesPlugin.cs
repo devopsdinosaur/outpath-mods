e05e476f2a9f03af3611379848deb0ce19cdb59c
devopsdinosaur <devopsdinosaur@gmail.com>
2023-12-10 00:18:32 -0500

    Added instant pickup of items and enemy spawn to more_resources

--- more_resources/MoreResourcesPlugin.cs
+++ more_resources/MoreResourcesPlugin.cs
@@ -63,15 +63,52 @@ public class MoreResourcesPlugin : BaseUnityPlugin {
 	[HarmonyPatch(typeof(TakeOutResource), "SpawnDrops")]
 	class HarmonyPatch_TakeOutResource_SpawnDrops {
 		
-		private static bool Prefix(ref float _mult) {
+		private static bool Prefix(TakeOutResource __instance) {
 			try {
-				if (m_enabled.Value) {
-					_mult = m_drop_multiplier.Value;
+				if (!m_enabled.Value) {
+					return true;
 				}
+				for (int i = 0; i < __instance.itemPoolGroups.Length; i++) {
+					if (!(UnityEngine.Random.value < __instance.itemPoolGroups[i].probToSpawn)) {
+						continue;
+					}
+					float num = UnityEngine.Random.Range(__instance.itemPoolGroups[i].quantityToSpawn.x, __instance.itemPoolGroups[i].quantityToSpawn.y) * m_drop_multiplier.Value * (__instance.mult_PropFromCrop * __instance.mult_IsTresure);
+					int num2 = Mathf.RoundToInt(num + (float)Mathf.RoundToInt(num * 0.3f * (float)UpgradesData.instance.GetUpgrade_Int(1)));
+					if (num2 >= 1) {
+						int num3 = num2 / __instance.itemPoolGroups[i].quantityItemDrops;
+						if (num3 <= 0) {
+							num3 = 1;
+						}
+						for (int j = 0; j < __instance.itemPoolGroups[i].quantityItemDrops; j++) {
+							InventoryManager.instance.AddItemToInv((ItemInfo) __instance.GetType().GetMethod("GetItemInfo", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(__instance, new object[] {i}), num3);
+						}
+					}
+				}
+				if (__instance.isRock) {
+					float num4 = UpgradesData.instance.GetUpgrade_Int(39);
+					num4 *= m_drop_multiplier.Value;
+					if (num4 >= 1f) {
+						InventoryManager.instance.AddItemToInv(ItemList.instance.itemList[4], (int) num4);
+					}
+				}
+				return false;
 			} catch (Exception e) {
 				logger.LogError($"** HarmonyPatch_TakeOutResource_SpawnDrops ERROR - {e}");
 			}
 			return true;
+		}
+	}
+
+	
+	[HarmonyPatch(typeof(EnemyHealth), "SpawnDrops")]
+	class HarmonyPatch_EnemyHealth_SpawnDrops {
+		
+		private static bool Prefix(EnemyHealth __instance, float mult) {
+			if (!m_enabled.Value) {
+				return true;
+			}
+			__instance.SpawnDropsAndPickup(mult * m_drop_multiplier.Value);
+			return false;
 		}
 	}
 }
--- testing/TestingPlugin.cs
+++ testing/TestingPlugin.cs
@@ -84,5 +84,4 @@ public class TestingPlugin : BaseUnityPlugin {
 		}
 	}
 
-	
 }
